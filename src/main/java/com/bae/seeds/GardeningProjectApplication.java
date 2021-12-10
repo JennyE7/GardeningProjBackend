a84b817eabeaab1ad4ccd866a7c4d36e73c97a63
JennyE7 <jenniferjayne.me@hotmail.com>
2021-12-10 15:15:29 +0000

    initial commit

---
+++ src/main/java/com/bae/seeds/GardeningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bae.seeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardeningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardeningProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bae/seeds/GardeningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.seeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardeningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
