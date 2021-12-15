8a080d8b8b5af4bd8b66dfea2c942108af26fa4d
JennyE7 <jenniferjayne.me@hotmail.com>
2021-12-15 16:18:27 +0000

    minor adjstments for integration with frontend

--- src/main/java/com/bae/seeds/repo/SeedRepo.java
+++ src/main/java/com/bae/seeds/repo/SeedRepo.java
@@ -1,9 +1,11 @@
 package com.bae.seeds.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.bae.seeds.domain.Seed;
 
+@Repository
 public interface SeedRepo extends JpaRepository<Seed, Integer> {
 
 }
